951c571967b3cc65a449d78c790d0ce9cb57d18e
dwesh163 <ehouarn.duriaux@protonmail.com>
2025-01-03 17:03:47 +0100

    Initial commit

---
+++ components/contexts/theme-provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
+  return <NextThemeProvider attribute="class">{children}</NextThemeProvider>;
+}
---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  name: "Launch UI",
+  url: "https://launchuicomponents.com",
+  getStartedUrl:
+    "https://launchuicomponents.com/docs/getting-started/introduction",
+  ogImage: "https://launchuicomponents.com/og.jpg",
+  description:
+    "Landing page components built with React, Shadcn/ui and Tailwind that will make your website feel premium.",
+  links: {
+    twitter: "https://twitter.com/mikolajdobrucki",
+    github: "https://github.com/launch-ui/launch-ui",
+  },
+  stats: {
+    updated: "2024-12-29",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
